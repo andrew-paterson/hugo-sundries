47987c5d922247a6acda91eeb0e7e37a6318ca60
andrew-paterson <andrew.paterson9@gmail.com>
2021-04-26 12:50:46 +0200

    Bugfix for responsive-image- check for clones using fill path.

--- js/image-gallery/gallery.js
+++ js/image-gallery/gallery.js
@@ -22,7 +22,20 @@ $('#wrapper').scroll(function() {
 });
 
 $("[data-fancybox='gallery']").fancybox({
+  
   idleTime: 9999999,
+  preventCaptionOverlap: false,
+  baseTpl:
+    '<div class="fancybox-container" role="dialog" tabindex="-1">' +
+    '<div class="fancybox-bg"></div>' +
+    '<div class="fancybox-inner">' +
+    '<div class="fancybox-infobar"><span data-fancybox-index></span>&nbsp;/&nbsp;<span data-fancybox-count></span></div>' +
+    '<div class="fancybox-toolbar">{{buttons}}</div>' +
+    '<div class="fancybox-navigation">{{arrows}}</div>' +
+    '<div class="fancybox-stage"><div class="fancybox-caption"><div class=""fancybox-caption__body"></div></div></div>' +
+    '' +
+    '</div>' +
+    '</div>',
 });
 
 function doGallery(galleryElement) {
