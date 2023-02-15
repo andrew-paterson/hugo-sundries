10f3387c12ecc12d7e1322475009d9ebe39821bf
Andrew Paterson <andrew.paterson9@gmail.com>
2023-02-15 16:02:11 +0200

    Add view switcher to auto accordions

--- js/accordion-js/auto-accordions.js
+++ js/accordion-js/auto-accordions.js
@@ -1,42 +1,55 @@
-const autoAccordionEls = Array.from(document.querySelectorAll('.auto-accordion'));
-autoAccordionEls.forEach(acc => {
-  const newAccGroup = document.createElement('div');
-  newAccGroup.classList.add('accordion-container');
-  newAccGroup.setAttribute('data-auto-scroll', true);
-  let currentAccItem;
-  const children = Array.from(acc.querySelectorAll(':scope > *'));
-  children.forEach((el, index) => {
-    if (el.tagName === 'H2') {
-      if (currentAccItem) {
-        newAccGroup.appendChild(currentAccItem);
-      }
-      currentAccItem = document.createElement('div');
-      currentAccItem.classList.add('ac');
-      currentAccItem.classList.add('js-enabled');
-      currentAccItem.setAttribute('data-title-parsed', el.textContent.replace(/\s/g, '-').replace(/-+/g, '-').replace(/[^0-9a-zA-Z_-]/g, '').toLowerCase());
-      const headerEl = document.createElement('div');
-      headerEl.classList.add('ac-header');
-      const triggerEl = document.createElement('div');
-      triggerEl.classList.add('ac-trigger');
-      triggerEl.appendChild(el);
-      headerEl.appendChild(triggerEl);
-      currentAccItem.appendChild(headerEl);
-      const panelEl = document.createElement('div');
-      panelEl.classList.add('ac-panel');
-      const panelInnerEl = document.createElement('div');
-      panelInnerEl.classList.add('ac-panel-inner');
-      panelEl.appendChild(panelInnerEl);
-      currentAccItem.appendChild(panelEl);
-    } else if (index === children.length - 1) {
-      currentAccItem.querySelector('.ac-panel-inner').appendChild(el);
-      newAccGroup.appendChild(currentAccItem);
-    } else {
-      if (currentAccItem) {
+const params = new Proxy(new URLSearchParams(window.location.search), {
+  get: (searchParams, prop) => searchParams.get(prop),
+});
+const viewSwitchLink = document.createElement('a');
+if (params.view !== 'plain') {
+  viewSwitchLink.setAttribute('href', '?view=plain');
+  viewSwitchLink.textContent = 'View as plain text';
+  const autoAccordionEls = Array.from(document.querySelectorAll('.auto-accordion'));
+  autoAccordionEls.forEach(acc => {
+    const newAccGroup = document.createElement('div');
+    newAccGroup.classList.add('accordion-container');
+    newAccGroup.setAttribute('data-auto-scroll', true);
+    let currentAccItem;
+    const children = Array.from(acc.querySelectorAll(':scope > *'));
+    children.forEach((el, index) => {
+      if (el.tagName === 'H2') {
+        if (currentAccItem) {
+          newAccGroup.appendChild(currentAccItem);
+        }
+        currentAccItem = document.createElement('div');
+        currentAccItem.classList.add('ac');
+        currentAccItem.classList.add('js-enabled');
+        currentAccItem.setAttribute('data-title-parsed', el.textContent.replace(/\s/g, '-').replace(/-+/g, '-').replace(/[^0-9a-zA-Z_-]/g, '').toLowerCase());
+        const headerEl = document.createElement('div');
+        headerEl.classList.add('ac-header');
+        const triggerEl = document.createElement('div');
+        triggerEl.classList.add('ac-trigger');
+        triggerEl.appendChild(el);
+        headerEl.appendChild(triggerEl);
+        currentAccItem.appendChild(headerEl);
+        const panelEl = document.createElement('div');
+        panelEl.classList.add('ac-panel');
+        const panelInnerEl = document.createElement('div');
+        panelInnerEl.classList.add('ac-panel-inner');
+        panelEl.appendChild(panelInnerEl);
+        currentAccItem.appendChild(panelEl);
+      } else if (index === children.length - 1) {
         currentAccItem.querySelector('.ac-panel-inner').appendChild(el);
+        newAccGroup.appendChild(currentAccItem);
       } else {
-        newAccGroup.appendChild(el);
+        if (currentAccItem) {
+          currentAccItem.querySelector('.ac-panel-inner').appendChild(el);
+        } else {
+          newAccGroup.appendChild(el);
+        }
       }
-    }
-    acc.appendChild(newAccGroup)
-  });
-})
+      acc.appendChild(newAccGroup)
+    });
+  })
+} else {
+  viewSwitchLink.setAttribute('href', '?view=interactive');
+  viewSwitchLink.textContent = 'Interactive version';
+}
+const viewSwitchLinkContainer = document.querySelector('.view-switch-links');
+viewSwitchLinkContainer.appendChild(viewSwitchLink);
