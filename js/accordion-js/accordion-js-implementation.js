bcef4741753f1ec5a9475f8828958bfcfe325e26
Andrew Paterson <andrew.paterson9@gmail.com>
2024-07-12 13:42:55 +0200

    Accordionjs implementation determines element to scroll dynamically

--- js/accordion-js/accordion-js-implementation.js
+++ js/accordion-js/accordion-js-implementation.js
@@ -14,55 +14,66 @@ const accordionEls = Array.from(document.querySelectorAll('.accordion-container'
 
 const hash = window.location.hash;
 if (hash) {
-  accordionEls.forEach(accordionEl => {
+  accordionEls.forEach((accordionEl) => {
     const showMultiple = accordionEl.hasAttribute('data-ac-show-multiple');
     const urlHash = `[data-title-parsed=${hash.replace('#', '')}]`;
     const urlHashAccordionPanel = accordionEl.querySelector(urlHash);
     if (urlHashAccordionPanel) {
       const urlHashAccordionPanelAncestors = ancestorsWithClass(urlHashAccordionPanel, 'ac');
-      urlHashAccordionPanelAncestors.forEach(ac => {
-        ac.setAttribute('data-ac-panel-initially-open', '');  
+      urlHashAccordionPanelAncestors.forEach((ac) => {
+        ac.setAttribute('data-ac-panel-initially-open', '');
       });
       if (!showMultiple) {
         const defaultOpen = accordionEl.querySelector('[data-ac-panel-default-open]');
         if (defaultOpen) {
-          defaultOpen.removeAttribute('data-ac-panel-default-open')
+          defaultOpen.removeAttribute('data-ac-panel-default-open');
         }
       }
     }
   });
 }
-const scrollElementSelector = '#wrapper';
+
+function getScrollParent(node) {
+  if (node == null) {
+    return null;
+  }
+
+  if (node.scrollHeight > node.clientHeight) {
+    return node;
+  } else {
+    return getScrollParent(node.parentNode);
+  }
+}
 
 function scrollElementToTop(currElement) {
   const parsedTitle = currElement.getAttribute('data-title-parsed');
   window.location.hash = parsedTitle;
   if (scrollElementSelector) {
-    const scrollElement = document.querySelector(scrollElementSelector);
+    const scrollElement = getScrollParent(currElement);
     const scrollElementRect = scrollElement.getBoundingClientRect();
     const scrollElementPaddingTop = parseFloat(window.getComputedStyle(scrollElement, null).getPropertyValue('padding-top'));
     const scrollValue = currElement.offsetTop - scrollElementRect.top - scrollElementPaddingTop - 12;
     scrollElement.scrollTop = scrollValue;
   } else {
-    currElement.scrollIntoView({behavior: 'smooth'});
+    currElement.scrollIntoView({ behavior: 'smooth' });
   }
 }
 
-const accordionObjects = accordionEls.map(accordionEl => {
+const accordionObjects = accordionEls.map((accordionEl) => {
   const options = {
     onOpen: (currElement) => {
       scrollElementToTop(currElement);
-    }
-  }
+    },
+  };
   const accordionPanelElements = Array.from(accordionEl.querySelectorAll(':scope > .ac'));
-  const accordionPanels = accordionPanelElements.map(item => {
+  const accordionPanels = accordionPanelElements.map((item) => {
     return {
       title: item.getAttribute('data-title-parsed'),
-      initiallyOpen: item.hasAttribute('data-ac-panel-initially-open') || item.hasAttribute('data-ac-panel-default-open') 
-    }
+      initiallyOpen: item.hasAttribute('data-ac-panel-initially-open') || item.hasAttribute('data-ac-panel-default-open'),
+    };
   });
 
-  const openOnInit = accordionPanels.filter(item => item.initiallyOpen).map(item => accordionPanels.indexOf(item));
+  const openOnInit = accordionPanels.filter((item) => item.initiallyOpen).map((item) => accordionPanels.indexOf(item));
   if (openOnInit.length) {
     options.openOnInit = openOnInit;
   }
@@ -73,33 +84,32 @@ const accordionObjects = accordionEls.map(accordionEl => {
     items: accordionPanels,
     options: options,
     openOnInit: openOnInit,
-  }
+  };
 });
 
-accordionEls.forEach(accordionEl => {
-  accordionEl.classList.add('opacity-0')
+accordionEls.forEach((accordionEl) => {
+  accordionEl.classList.add('opacity-0');
 });
 
 window.onload = (event) => {
-  accordionObjects.forEach(accordionObject => {
+  accordionObjects.forEach((accordionObject) => {
     new Accordion(accordionObject.accordionEl, accordionObject.options);
     const autoScrollEl = document.querySelector('[data-auto-scroll] [data-ac-panel-initially-open]');
     if (autoScrollEl) {
       scrollElementToTop(autoScrollEl);
     }
   });
-  accordionEls.forEach(accordionEl => {
+  accordionEls.forEach((accordionEl) => {
     accordionEl.classList.remove('opacity-0');
     const links = accordionEl.querySelectorAll('[data-accordion-link]');
     for (let i = 0; i < links.length; i++) {
       const link = links[i];
-      link.addEventListener('click', function(event) {
+      link.addEventListener('click', function (event) {
         event.preventDefault();
         const attr = `[data-title-parsed=${link.getAttribute('href').replace('#', '')}]`;
         const targetElement = document.querySelector(attr);
         targetElement.querySelector('.ac-trigger').click();
-
-    })
+      });
     }
-  })
+  });
 };
